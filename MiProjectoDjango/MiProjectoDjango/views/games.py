bfa2af7b8de6514cc362d46e681f059de34639b2
Elkin Panameño <elkinsamir643@gmail.com>
2021-11-04 18:25:07 -0500

    Initial Commit

---
+++ MiProjectoDjango/MiProjectoDjango/views/games.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+import datetime
+from django.template import loader
+from django.template import Template, Context
+
+def games(request):
+    year = datetime.datetime.now().year
+    day = datetime.datetime.now().day
+    month = datetime.datetime.now().month
+    hour = datetime.datetime.now().strftime("%X")
+
+    fecha = "%s/%s/%s a las %s" %(day, month, year, hour)
+
+    external_doc = loader.get_template('games.html')
+    document = external_doc.render({'fecha': fecha})
+    return HttpResponse(document)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Yotube
+# YoTube 
+**YoTube** es el nombre de un proyecto que hice para MisionTic 2022 - Ruta 1 el cual consiste en ser la version barata de YouTube 🤣
+
+  <div align="center"> 
+	<img src="https://i.ibb.co/17G2Dqp/yotube.png" alt="YouTube">
+  </div>
+
+ - Correr en servidor local: 
+
+     > **cd MiProjectoDjango/** 
+	 > **python manage.py runserver**
+
+## Version 0.1
+- Mejoras en los estilos CSS
+- Mejora en la documentacion del codigo
